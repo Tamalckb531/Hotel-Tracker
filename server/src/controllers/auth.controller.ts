9935cb29cb1e0d1362fd72e50350b06e8d51236e
Tamal <ckbtamaldipnew@gmail.com>
2024-07-07 10:55:16 +0600

    add login controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,30 +1,46 @@
 import { z } from 'zod';
 import bcryptjs from 'bcryptjs';
 import { PrismaClient } from '@prisma/client'
+import { User } from '@prisma/client';
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 
-interface bodyProps{
+//? --------- keys,types and interfaces --------- 
+interface SignUpBodyProps{
     firstName : string,
     lastName : string,
     email : string,
     password : string,
 }
-const SignUpSchema = z.object({
-    firstName: z.string().min(3).max(10),
-    lastName: z.string().min(3).max(10),
+type LoginBodyProps = Pick<SignUpBodyProps, 'email' | 'password'>;
+
+const secretKey = 'my-secret-key';
+//? --------- types and interfaces --------- 
+
+//? --------- Zod Schema --------- 
+
+const BaseAuthSchema = z.object({
     email: z.string().email(),
     password: z.string().min(5)
-})
+});
+
+const SignUpSchema = BaseAuthSchema.extend({
+    firstName: z.string().min(3).max(10),
+    lastName: z.string().min(3).max(10),
+});
+
+const LoginSchema = BaseAuthSchema;
+//? --------- Zod Schema --------- 
 
 export const signupProcess = async(req: any, res: any) => {
-    const { firstName, lastName, email, password }: bodyProps = req.body;
+    const { firstName, lastName, email, password }: SignUpBodyProps = req.body;
     
     try {
         SignUpSchema.parse({ firstName, lastName, email, password });
 
         //? email already exist
-        const existingUser = await prisma.user.findUnique({
+        const existingUser:User|null = await prisma.user.findUnique({
             where: { email }
         });
         if (existingUser) {
@@ -33,9 +49,9 @@ export const signupProcess = async(req: any, res: any) => {
             })
         }
 
-        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const hashedPassword:string = bcryptjs.hashSync(password, 10);
 
-        const newUser = await prisma.user.create({
+        const newUser:User = await prisma.user.create({
             data: { 
                 firstName,
                 lastName,
@@ -44,7 +60,22 @@ export const signupProcess = async(req: any, res: any) => {
             }
         })
 
-        res.json({ msg: "Sign-up successful", user:newUser });
+        //? Token Generation with JWT and sending as cookie
+
+        const token:string = jwt.sign(
+            { id: newUser.id },
+            secretKey
+        )
+
+        const { password: pass, ...userWithoutPass } = newUser;
+
+        
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json({
+            msg: "Sign-up Successful",
+            user: userWithoutPass
+        });
     } catch (error: any) {
         
         if (error instanceof z.ZodError) {
@@ -59,6 +90,58 @@ export const signupProcess = async(req: any, res: any) => {
 }
 
 
-export const loginProcess = (req: any, res: any) => {
-    res.json({ msg: "Login is working fine" })
+export const loginProcess = async(req: any, res: any) => {
+    const { email, password }: LoginBodyProps = req.body;
+    
+    try {
+
+        //? Validation via zod
+        LoginSchema.parse({ email, password });
+
+        //? Getting the user via email
+        const validUser: User | null = await prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (!validUser) {
+            return res.status(404).json({
+                msg: "Email not registered"
+            })
+        }
+
+        //? Password validation with bcryptJs
+        const validPassword: boolean = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) {
+            return res.status(404).json({
+                msg: "Invalid Password"
+            })
+        }
+
+        //? Token Generation with JWT and sending as cookie
+
+        const token:string = jwt.sign(
+            { id: validUser.id },
+            secretKey
+        )
+
+        const { password: pass, ...userWithoutPass } = validUser;
+
+        
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json({
+            msg: "Log-in Successful",
+            user: userWithoutPass
+        });
+    } catch (error: any) {
+        
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                msg: "Invalid input data",
+                errors: error.errors
+            });
+        }
+        res.status(500).json({msg: error.message})
+    }
 }
